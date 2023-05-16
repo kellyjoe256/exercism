217885e786f3dc097158532d95dc759d26f97606
Wafukho Kelly Joseph <kellyjoe256@gmail.com>
2023-05-16 08:01:06 +0300

    bash improved secret handshake

--- bash/secret-handshake/secret_handshake.sh
+++ bash/secret-handshake/secret_handshake.sh
@@ -1,47 +1,30 @@
 #!/usr/bin/env bash
 
-convert_to_binary () {
-    binary=''
-    number="$1"
-    while (( number > 0 ))
-    do
-        binary=$(( number % 2 ))$binary
-        number=$(( number / 2 ))
-    done
-
-    printf '%05s' "${binary}"
-}
-
 main () {
     if (( $1 == 0 ))
     then
         return 0
     fi
 
-    local binary
-    local range
-    local output=''
+    local -a output=()
+    local -r number=$1
     local -ra handshakes=('jump' 'close your eyes' 'double blink' 'wink')
-    binary=$(convert_to_binary "${1}")
-    binary=${binary// /0}
-    [[ "${binary:0:1}" == '1' ]] && range="{0..3}" || range="{3..0}"
-    for index in $(eval echo "${range}")
+    local number_of_bits=$(( (${#handshakes[@]} + 1) - 1 ))
+    local -r initial_bit=$(( (number >> number_of_bits) & 1 ))
+    number_of_bits=$(( number_of_bits - 1 ))
+    for index in "${!handshakes[@]}"
     do
-        bit="${binary:index+1:1}"
-        handshake="${handshakes[index]}"
-        if [[ "${bit}" != '0' ]]
+        (( initial_bit == 1 )) && temp=$(( number_of_bits - index )) || temp=$index
+        bit=$(( (number >> temp) & 1 ))
+        if (( bit == 1 ))
         then
-           output+="${handshake},"
+            (( initial_bit == 1 )) && subscript=$index || subscript=$(( (index * -1) + -1 ))
+            output+=("${handshakes[subscript]}")
         fi
     done
 
-    if [[ "${output}" == '' ]]
-    then
-        return 0
-    fi
-
-    # remove last comma
-    echo "${output::-1}"
+    local IFS=','
+    echo "${output[*]}"
 }
 
 main "$@"
